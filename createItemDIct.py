99d32c255049537446b932548e39ab0e10d96312
ejpellot <pellote5@students.rowan.edu>
2019-05-20 11:52:04 -0400

    First commit

---
+++ createItemDIct.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+
+
+def createItemDict(url, name): # Returns Collection of item
+    r = requests.get(url, headers=agent)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    item_dic = {}
+    item_collection = soup.select(".product-item")
+    
+    for item in item_collection:
+        if item.select(".item-heading"):
+            product_page = item.select(".item-link")[0]['href']
+            item_link = "https://www2.hm.com" + (product_page)
+            style = item.select(".link")[0].text.strip()
+            price = item.select(".item-price")[0].text.strip().replace("$","")
+            new_price = float(price)
+            img = "https:" + item.find('img')['data-src']
+            altimg = "https:" + item.find('img')['data-altimage']
+            item_id =  item.find('article')['data-articlecode']
+            print(item_link)
+            temp= {
+                "id": item_id,
+                "product_page": item_link,
+                "style": style,
+                "price": new_price,
+                "image": [img, altimg]
+                }
+            item_dic.setdefault(name,[]) ##fe
+            item_dic[name].append(temp)
+    return item_dic
---
+++ test.py
@@ -0,0 +1,17 @@
+import populateItem as p
+
+hm_array = []
+url = "https://www2.hm.com/en_us/men/products/jeans.html?page-size=1000"
+
+p.createItemDict(url,"Jeans")
+
+
+# jean_arr.append(jean_dic)
+
+# In this mini project I learned how to scrape data from 
+# a clothing website using beaitufulsoup and store data using 
+# a dictionary which then converted to a json object.
+# Inside this json object there holds the jean key which
+# holds different types of jeans and there info
+
+# Then this data can be parsed for personal uses.
